f950f2f1fab6b759b14c10ee94c635a05af24c69
kmsoooooo1 <57782187+kmsoooooo1@users.noreply.github.com>
2020-04-27 11:54:54 +0900

    add team2_board

--- Team2/temp_DB/team2_DB_1.0.sql
+++ Team2/temp_DB/team2_DB_1.0.sql
@@ -2,7 +2,7 @@ CREATE DATABASE  IF NOT EXISTS `team2_project` /*!40100 DEFAULT CHARACTER SET ut
 USE `team2_project`;
 -- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
 --
--- Host: localhost    Database: team2_project
+-- Host: 127.0.0.1    Database: team2_project
 -- ------------------------------------------------------
 -- Server version	5.6.45-log
 
@@ -52,6 +52,39 @@ LOCK TABLES `team2_animals` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `team2_board`
+--
+
+DROP TABLE IF EXISTS `team2_board`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `team2_board` (
+  `b_idx` int(11) NOT NULL AUTO_INCREMENT,
+  `b_category` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `b_p_cate` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `b_title` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `b_writer` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `b_content` varchar(2000) COLLATE utf8_unicode_ci NOT NULL,
+  `b_ref` int(11) NOT NULL,
+  `b_like` int(11) DEFAULT '0',
+  `b_view` int(11) DEFAULT '0',
+  `b_reg_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `ip_addr` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `b_file` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`b_idx`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `team2_board`
+--
+
+LOCK TABLES `team2_board` WRITE;
+/*!40000 ALTER TABLE `team2_board` DISABLE KEYS */;
+/*!40000 ALTER TABLE `team2_board` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Dumping events for database 'team2_project'
 --
 
@@ -68,4 +101,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-04-27 11:39:42
+-- Dump completed on 2020-04-27 11:54:03
