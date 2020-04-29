b862c63dae15fbf339656784d09ebf8e7ccd2269
coduo25 <32178386+coduo25@users.noreply.github.com>
2020-04-29 15:33:15 +0900

    commit - JH

--- Team2/WebContent/include/header.jsp
+++ Team2/WebContent/include/header.jsp
@@ -25,5 +25,28 @@
 			<a href=""> 로그아웃 </a>
 		<% } %> 
 	</div>
+	
+	<a href="./notice.bo?category=notice">공지사항</a>
+	<a href="./review.bo?category=review">상품후기</a>
+	
+	<!-- 메인 메뉴 -->
+	<div>
+		<ul>
+			<li> <a href="./AnimalList.an?category=파충류"> 파충류 </a>
+				<ul>
+					<li> <a href="./AnimalList.an?category=파충류&sub_category=게코 도마뱀"> 게코 도마뱀 </a> </li>
+					<li> <a href="./AnimalList.an?category=파충류&sub_category=뱀"> 뱀 </a> </li>
+					<li> <a href="./AnimalList.an?category=파충류&sub_category=거북"> 거북 </a> </li>
+				</ul>
+			</li>
+			<li> <a href="./AnimalList.an?category=양서류"> 양서류 </a>
+				<ul>
+					<li> <a href="./AnimalList.an?category=양서류&sub_category=프로그"> 프로그 </a> </li>
+					<li> <a href="./AnimalList.an?category=양서류&sub_category=살라맨더"> 살라맨더 </a> </li>
+					<li> <a href="./AnimalList.an?category=양서류&sub_category=팩맨"> 팩맨 </a> </li>
+				</ul>
+			</li>
+		</ul>
+	</div>
 </body>
 </html>
---
+++ Team2/src/team2/animal/action/AnimalListAction.java
@@ -0,0 +1,39 @@
+package team2.animal.action;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import team2.animal.db.AnimalDAO;
+import team2.animal.db.AnimalDTO;
+
+public class AnimalListAction implements Action{
+	
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		//AnimalDAO() 객체 생성
+		AnimalDAO adao = new AnimalDAO();
+		
+		//구분짓기 위한 변수 받기
+		String category = request.getParameter("category");
+		String sub_category = request.getParameter("sub_category");
+		if(sub_category == null) {
+			//만약 유저가 '파충류'나 '양서류'만 선택했을시 sub_category에는 All을 넣어서 모든 파충류 또는 양서류를 가지고 온다.
+			sub_category = "all"; 
+		}
+		
+		List<AnimalDTO> animalList = adao.getAnimalList(category, sub_category); 
+		
+		//등록된 모든 상품의 정보를 가져와서 request 영역에 저장
+		request.setAttribute("animalList", animalList);
+		
+		ActionForward forward = new ActionForward();
+		forward.setPath("./animals/animals_list.jsp");
+		forward.setRedirect(false); //주소가 바뀌지 않고 리스트를 보여줘야 하기 때문에
+
+		return forward;
+	}
+
+}
