534eb5b1bd23278fc53a573d6751a542b1bdf38a
kmsoooooo1 <57782187+kmsoooooo1@users.noreply.github.com>
2020-05-01 12:10:07 +0900

    cSet Add and footer.jsp change
    cSet index로 표현해서 select태그 작성 시 value 숫자로 표현할 예정

--- Team2/src/team2/board/action/cSet.java
+++ Team2/src/team2/board/action/cSet.java
@@ -3,4 +3,39 @@ package team2.board.action;
 public class cSet {
 	public static final String[] Category = {"카테고리1","카테고리2","카테고리3"};
 	public static final String[] p_Category = {"세부카테1","세부카테2","세부카테3","세부카테4"};
+	
+	//	Category index
+	private int c;
+	
+	//	p_Category index
+	private int pc;
+	
+	public cSet() {}
+
+	public cSet(int c, int pc) {
+		super();
+		this.c = c;
+		this.pc = pc;
+	}
+
+	public int getC() {
+		return c;
+	}
+
+	public void setC(int c) {
+		this.c = c;
+	}
+
+	public int getPc() {
+		return pc;
+	}
+
+	public void setPc(int pc) {
+		this.pc = pc;
+	}
+
+	@Override
+	public String toString() {
+		return "Selected Now : cSet [Category=" + Category[c] + ", p_Category=" + p_Category[pc] + "]";
+	}
 }
