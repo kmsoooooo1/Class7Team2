6ae12cfee411ac9b7932655b27e5c82dd6f3c30c
ITWILL <ITWILL@DESKTOP-6181D4M>
2020-05-08 15:33:02 +0900

    commit - H

--- Team2/WebContent/goods/goods_detail.jsp
+++ Team2/WebContent/goods/goods_detail.jsp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <%@page import="java.net.URLEncoder"%>
 <%@page import="java.util.List"%>
 <%@page import="java.util.Arrays"%>
@@ -851,29 +850,4 @@
 	
 </script>
 
-
-
-
-=======
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<script src="http://code.jquery.com/jquery-latest.min.js"></script>
-<script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
-<title>Insert title here</title>
-</head>
-<body>
-	<h1>성공</h1>
-
-
-
-
-
-
-
-</body>
->>>>>>> refs/heads/master
 </html>
--- Team2/src/team2/goods/action/GoodsDetailAction.java
+++ Team2/src/team2/goods/action/GoodsDetailAction.java
@@ -1,6 +1,6 @@
 package team2.goods.action;
 
-<<<<<<< HEAD
+
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -26,33 +26,7 @@ public class GoodsDetailAction implements Action{
 		List<GoodsDTO> detailList = gdao.getGoodsDetailList(g_code);
 		
 		request.setAttribute("detailList", detailList);
-=======
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import team2.goods.db.GoodsDAO;
-import team2.goods.db.GoodsDTO;
-
-public class GoodsDetailAction implements Action{
-
-	@Override
-	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
-		// 넘겨준 삼품 코드 값 저장
-		String g_code = request.getParameter("g_code");
-		
-		GoodsDAO gdao = new GoodsDAO();
-		
-		// 상품 페이지 조회수 1업하는 함수 호출
-		gdao.updateGoodsViewCount(g_code);
-		
-		// 상품 세부정보 가져오기
-		GoodsDTO gdto = gdao.getGoodsDetail(g_code);
-		
-		request.setAttribute("goodsDetail", gdto);
->>>>>>> refs/heads/master
-		
-		
 		ActionForward forward = new ActionForward();
 		forward.setPath("./goods/goods_detail.jsp");
 		forward.setRedirect(false);
